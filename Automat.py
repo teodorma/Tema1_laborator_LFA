97e327a5461803d17f7a5ff783b56baf0ec6f981
Teodor Maciuca <108132918+teodorma@users.noreply.github.com>
2023-03-28 11:23:47 +0300

    Update Automat.py

--- Automat.py
+++ Automat.py
@@ -5,19 +5,25 @@ f.close()
 stareFinala  = ''
 stareCurenta = 'q0'
 drum = []
+aux = 0
 cuvant = input("Cuvantul citit este:")
-ok = 0
 
 for x in cuvant:
+    ok = 0
     for i in range(len(l)-1):
         if stareCurenta == l[i][0] and x == l[i][1] and ok == 0:
             drum.append(stareCurenta)
             stareCurenta = l[i][2]
             stareFinala = l[i][2]
+            ok+=1
             break
-          
-if stareFinala in l[len(l)-1]:
-    print("Cuvantul "+cuvant+" nu este acceptat")
+    if ok == 0:
+        aux = 1
+        break
+        
+drum.append(stareFinala)
+if stareFinala in l[len(l)-2] and aux == 0:
+    print("Cuvantul "+cuvant+" este acceptat")
     print("Drumul este: "+str(drum))
 else:
     print("Cuvant neacceptat")
